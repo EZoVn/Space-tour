bf9a5ceaa72bc8f74e616bbb6a1635ee143b24e1
EzoVn <vincent.galpin@gmail.com>
2024-02-21 08:41:38 +0100

    ajout page crew

--- src/components/Crew.vue
+++ src/components/Crew.vue
@@ -5,14 +5,20 @@
         <span class="font-bold text-gray-400">02</span> Meet your crew
       </h1>
 
-      <section class="flex justify-between">
-        <div>
-          <h2 class="opacity-25 uppercase text-4xl">{{ dataCrew[`${selected}`].role }}</h2>
-          <h2>{{ dataCrew[`${selected}`].name }}</h2>
-          <h2>{{ dataCrew[`${selected}`].bio }}</h2>
+      <section class="container mt-32">
+        <div class="w-1/2">
+          <h2 class="opacity-25 uppercase text-4xl">{{ dataCrew[`${selectedCrew}`].role }}</h2>
+          <p class="text-6xl uppercase pt-4 pb-8">{{ dataCrew[`${selectedCrew}`].name }}</p>
+          <p>{{ dataCrew[`${selectedCrew}`].bio }}</p>
+          <div>
+            <span @click="toggleActiveCrew(0)" class="spany" :class="{isActive: selectedCrew === 0}"></span>
+            <span @click="toggleActiveCrew(1)" class="spany" :class="{isActive: selectedCrew === 1}"></span>
+            <span @click="toggleActiveCrew(2)" class="spany" :class="{isActive: selectedCrew === 2}"></span>
+            <span @click="toggleActiveCrew(3)" class="spany" :class="{isActive: selectedCrew === 3}"></span>
+          </div>
         </div>
         <div>
-          <img class="absolute bottom-0 right-20 w-[568px] h-[712px]" :src="dataCrew[`${selected}`].images.webp" alt="Photo du pilote">
+          <img class="absolute bottom-0 right-20" :src="dataCrew[`${selectedCrew}`].images.webp" alt="Photo du pilote">
         </div>
       </section>
     </div>
@@ -24,25 +30,35 @@ import data from "../assets/data.json";
 import { ref } from "vue";
 
 const dataCrew = data.crew;
-const selected = ref(0);
+// const selected = ref(0);
+const selectedCrew = ref(0);
+
+function toggleActiveCrew(index) {
+  selectedCrew.value = index;
+}
 
 </script>
 
-<style>
+<style scoped>
 .background-crew {
   background-image: url("@/assets/crew/background-crew-desktop.jpg");
 }
-/* Commander */
+.spany{
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border-radius: 99%;
+  margin-right: 18px;
+  margin-top: 120px;
+  @apply bg-gray-400;
+}
+.spany:hover{
+  @apply bg-white opacity-25 cursor-pointer;
+}
+.isActive{
+  @apply bg-white;
+}
 
-/* image-removebg-preview(289) */
-
-position: absolute;
-width: 568.07px;
-height: 712px;
-left: 734.93px;
-top: 198px;
-
-background: url(Image.png);
 
 
 </style>
